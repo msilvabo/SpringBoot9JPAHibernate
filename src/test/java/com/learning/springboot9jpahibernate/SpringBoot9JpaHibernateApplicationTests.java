1fba991f674df3d85cc574128560a30cbe56eecc
Moises Silva Cardenas <moises.silva@et.bo>
2024-07-22 11:18:39 -0400

    inicia proyecto

---
+++ src/test/java/com/learning/springboot9jpahibernate/SpringBoot9JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springboot9jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot9JpaHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
